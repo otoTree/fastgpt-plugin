b7c33bf27b4c6af1f269d156839d65653c1008fd
Archer <545436317@qq.com>
2025-10-20 21:49:58 +0800

    update bucketname (#244)
    * add log
    
    * fix: upload file
    
    * update bucketname
    
    * rename bucket
    
    * rename bucket

--- src/s3/index.ts
+++ src/s3/index.ts
@@ -7,7 +7,7 @@ export const fileUploadS3Server = (() => {
         ? parseInt(process.env.MAX_FILE_SIZE)
         : 20 * 1024 * 1024, // 默认 20MB
       retentionDays: process.env.RETENTION_DAYS ? parseInt(process.env.RETENTION_DAYS) : 15, // 默认保留15天
-      bucket: process.env.S3_TOOL_BUCKET || 'fastgpt-tool',
+      bucket: process.env.S3_PUBLIC_BUCKET || 'fastgpt-public',
       externalBaseUrl: process.env.S3_EXTERNAL_BASE_URL,
       isPublicRead: true
     });
@@ -20,7 +20,7 @@ export const pluginFileS3Server = (() => {
   if (!global._pluginFileS3Server) {
     global._pluginFileS3Server = new S3Service({
       maxFileSize: 50 * 1024 * 1024, // 默认 50MB
-      bucket: process.env.S3_PLUGIN_BUCKET || 'fastgpt-plugin',
+      bucket: process.env.S3_PRIVATE_BUCKET || 'fastgpt-private',
       externalBaseUrl: process.env.S3_EXTERNAL_BASE_URL,
       isPublicRead: false
     });
